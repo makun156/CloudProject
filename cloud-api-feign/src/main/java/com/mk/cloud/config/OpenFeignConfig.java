9e7e844fbd06da1b36c5d8da7b69a314f73b11f8
makun <894214654@163.com>
2024-03-12 22:53:09 +0800

    ADD 开启openfeign自带的日志打印功能

--- cloud-api-feign/src/main/java/com/mk/cloud/config/OpenFeignConfig.java
+++ cloud-api-feign/src/main/java/com/mk/cloud/config/OpenFeignConfig.java
@@ -1,5 +1,6 @@
 package com.mk.cloud.config;
 
+import feign.Logger;
 import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +10,21 @@ public class OpenFeignConfig {
     //重试机制
     @Bean
     public Retryer retryer(){
-        /**
+        /** 失败重试机制
          * 1000表示重试的间隔时间（毫秒）
          * 5000表示最大重试间隔时间（毫秒）
          * 3表示最大重试次数
          */
         //return new Retryer.Default(1000, 5000, 3);
         return Retryer.NEVER_RETRY;
+    }
+
+    /**
+     * openfeign打印日志
+     * @return
+     */
+    @Bean
+    Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
     }
 }
